f6c38003cc723a3fcde35e045c300c66fe85c225
LJ Sonic <lamr@free.fr>
2024-09-15 19:33:49 +0200

    Update action animation format

--- PK3/Lua/ActionAnimations/Shake.lua
+++ PK3/Lua/ActionAnimations/Shake.lua
@@ -1,18 +1,18 @@
 itemapi.addActionAnimation("shake", {
-	start = function(_, action)
-		action.shakeOffset = 0
+	start = function(_, state)
+		state.shakeOffset = 0
 	end,
 
-	tick = function(mo, action)
+	tick = function(mo, state)
 		local intensity = 4*FU
 		local speed = TICRATE/8
 		local offset = itemapi.sinCycle(leveltime, -intensity, intensity, speed)
 
-		mo.spritexoffset = $ + offset - action.shakeOffset
-		action.shakeOffset = offset
+		mo.spritexoffset = $ + offset - state.shakeOffset
+		state.shakeOffset = offset
 	end,
 
-	stop = function(mo, action)
-		mo.spritexoffset = $ - action.shakeOffset
+	stop = function(mo, state)
+		mo.spritexoffset = $ - state.shakeOffset
 	end
 })
