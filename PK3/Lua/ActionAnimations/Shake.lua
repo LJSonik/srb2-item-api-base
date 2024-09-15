162acfcbb03cd506f42c2e4bd10dd32e887aae0c
LJ Sonic <lamr@free.fr>
2024-09-15 19:36:13 +0200

    Make shake animation configurable

--- PK3/Lua/ActionAnimations/Shake.lua
+++ PK3/Lua/ActionAnimations/Shake.lua
@@ -3,9 +3,9 @@ itemapi.addActionAnimation("shake", {
 		state.shakeOffset = 0
 	end,
 
-	tick = function(mo, state)
-		local intensity = 4*FU
-		local speed = TICRATE/8
+	tick = function(mo, state, params)
+		local intensity = params.intensity or (2*FU)
+		local speed = params.speed or (TICRATE/8)
 		local offset = itemapi.sinCycle(leveltime, -intensity, intensity, speed)
 
 		mo.spritexoffset = $ + offset - state.shakeOffset
