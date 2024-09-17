6e9959a19f941c005613c9b538db47d0202612df
LJ Sonic <lamr@free.fr>
2024-09-17 20:43:18 +0200

    Use templates for GFZ berry bushes

--- PK3/Lua/GreenflowerBushes.lua
+++ PK3/Lua/GreenflowerBushes.lua
@@ -1,15 +1,79 @@
-itemapi.addItem("berry", {
-	name = "berry",
-	template = "food",
-	stackable = 4,
+itemapi.addItemTemplate("berry_bush", function(def)
+	return {
+		groundAction1 = {
+			name = "pick berries",
+			duration = TICRATE,
+			animation = "shake",
 
-	nutrition = 60*TICRATE,
-	eatDuration = TICRATE/2,
-	foodCrumbColor = SKINCOLOR_RED,
+			action = function(p, bush)
+				if not p.itemapi_inventory:add(def.berry, 4) then return end
+				itemapi.spawnGroundItem(bush.x, bush.y, bush.z, def.leafedBush)
+				P_RemoveMobj(bush)
+			end
+		}
+	}
+end)
 
-	mobjSprite = SPR_IFOD,
-	mobjFrame = D
-})
+itemapi.addItemTemplate("leafed_berry_bush", function(def)
+	return {
+		template = "growing_plant",
+
+		growthTime = 3*60*TICRATE,
+
+		groundAction1 = {
+			name = "pick leaves",
+			duration = TICRATE/2,
+			animation = "shake",
+
+			action = function(p, bush)
+				if not p.itemapi_inventory:add(def.leaves) then return end
+				itemapi.spawnGroundItem(bush.x, bush.y, bush.z, def.leaflessBush)
+				P_RemoveMobj(bush)
+			end
+		}
+	}
+end)
+
+itemapi.addItemTemplate("leafless_berry_bush", function(def)
+	return {
+		template = "growing_plant",
+
+		growthTime = 3*60*TICRATE,
+
+		groundAction1 = {
+			name = "pick wood",
+			duration = TICRATE/2,
+			animation = "shake",
+
+			action = function(p, bush)
+				if not p.itemapi_inventory:add("log") then return end
+				itemapi.spawnGroundItem(bush.x, bush.y, bush.z, def.trunk)
+				P_RemoveMobj(bush)
+			end
+		}
+	}
+end)
+
+itemapi.addItemTemplate("berry_bush_trunk", function(def)
+	return {
+		template = "growing_plant",
+
+		growthTime = 3*60*TICRATE,
+
+		groundAction1 = {
+			name = "destroy and get seeds",
+			duration = TICRATE/2,
+			animation = "shake",
+
+			action = function(p, bush)
+				local n = P_RandomRange(1, 2)
+				if not p.itemapi_inventory:add(def.seed, n) then return end
+				P_RemoveMobj(bush)
+			end
+		}
+	}
+end)
+
 
 itemapi.addItem("blue_berry", {
 	name = "blueberry",
@@ -39,97 +103,67 @@ itemapi.addItem("leaves", {
 	}
 })
 
+
+itemapi.addItem("berry", {
+	name = "berry",
+	template = "food",
+	stackable = 4,
+
+	nutrition = 60*TICRATE,
+	eatDuration = TICRATE/2,
+	foodCrumbColor = SKINCOLOR_RED,
+
+	mobjSprite = SPR_IFOD,
+	mobjFrame = D
+})
+
 itemapi.addItem("berry_bush", {
 	name = "berry bush",
+	template = "berry_bush",
+
+	berry = "berry",
+	leafedBush = "leafed_berry_bush",
 
 	mobjType = MT_BERRYBUSH,
 	mobjSprite = SPR_BUS1,
-	mobjFrame = A,
-
-	groundAction1 = {
-		name = "pick berries",
-		duration = TICRATE,
-		animation = "shake",
-
-		action = function(p, bush)
-			if not p.itemapi_inventory:add("berry", 4) then return end
-			itemapi.spawnGroundItem(bush.x, bush.y, bush.z, "leafed_berry_bush")
-			P_RemoveMobj(bush)
-		end
-	}
+	mobjFrame = A
 })
 
 itemapi.addItem("leafed_berry_bush", {
 	name = "leafed berry bush",
-	template = "growing_plant",
+	template = "leafed_berry_bush",
 
 	groups = { growing_plant="berry_bush" },
-	growthTime = 3*60*TICRATE,
 	seed = "berry_bush_seed",
+	leaves = "leaves",
+	leaflessBush = "leafless_berry_bush",
 
 	mobjType = MT_BUSH,
 	mobjSprite = SPR_BUS2,
-	mobjFrame = A,
-
-	groundAction1 = {
-		name = "pick leaves",
-		duration = TICRATE/2,
-		animation = "shake",
-
-		action = function(p, bush)
-			if not p.itemapi_inventory:add("leaves") then return end
-			itemapi.spawnGroundItem(bush.x, bush.y, bush.z, "leafless_berry_bush")
-			P_RemoveMobj(bush)
-		end
-	}
+	mobjFrame = A
 })
 
 itemapi.addItem("leafless_berry_bush", {
 	name = "leafless berry bush",
-	template = "growing_plant",
+	template = "leafless_berry_bush",
 
 	groups = { growing_plant="leafed_berry_bush" },
-	growthTime = 3*60*TICRATE,
 	seed = "berry_bush_seed",
+	trunk = "berry_bush_trunk",
 
 	mobjSprite = SPR_ITEM,
-	mobjFrame = K,
-
-	groundAction1 = {
-		name = "pick wood",
-		duration = TICRATE/2,
-		animation = "shake",
-
-		action = function(p, bush)
-			if not p.itemapi_inventory:add("log") then return end
-			itemapi.spawnGroundItem(bush.x, bush.y, bush.z, "berry_bush_trunk")
-			P_RemoveMobj(bush)
-		end
-	}
+	mobjFrame = K
 })
 
 itemapi.addItem("berry_bush_trunk", {
 	name = "berry bush trunk",
-	template = "growing_plant",
+	template = "berry_bush_trunk",
 
 	groups = { growing_plant="leafless_berry_bush" },
-	growthTime = 3*60*TICRATE,
 	seed = "berry_bush_seed",
 
 	mobjSprite = SPR_ITEM,
-	mobjFrame = J,
-
-	groundAction1 = {
-		name = "destroy and get seeds",
-		duration = TICRATE/2,
-		animation = "shake",
-
-		action = function(p, bush)
-			local n = P_RandomRange(1, 2)
-			if not p.itemapi_inventory:add("berry_bush_seed", n) then return end
-			P_RemoveMobj(bush)
-		end
-	}
+	mobjFrame = J
 })
 
 itemapi.addItem("berry_bush_seed", {
