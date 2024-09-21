233ab7b08fd3aced55df69a3def915230177eb4b
LJ Sonic <lamr@free.fr>
2024-09-21 19:41:54 +0200

    Add GFZ tree

--- PK3/Lua/Trees.lua
+++ PK3/Lua/Trees.lua
@@ -1,34 +1,75 @@
-itemapi.addItem("greenflower_tree", {
-	name = "Greenflower tree",
-	carriable = false,
-
-	mobjSprite = SPR_TRE1,
-	mobjFrame = A,
-})
-
 freeslot("S_ITEMAPI_GFZTREE_LEAFLESS")
 states[S_ITEMAPI_GFZTREE_LEAFLESS] = { SPR_ITRE, A }
 
--- freeslot("S_ITEMAPI_GFZTREE_TRUNK")
--- states[S_ITEMAPI_GFZTREE_TRUNK] = { SPR_ITRE, A }
 
--- itemapi.addMobjAction(MT_BERRYBUSH, {
--- 	name = "pick berries",
+itemapi.addItem("greenflower_berry_tree", {
+	name = "Greenflower berry tree",
+	carriable = false,
 
--- 	action = function(p, tree)
--- 		if not p.itemapi_inventory:add("berry", 4) then return end
--- 		P_SpawnMobj(tree.x, tree.y, tree.z, MT_BUSH)
--- 		P_RemoveMobj(tree)
--- 	end
--- })
+	mobjType = MT_GFZBERRYTREE,
+	mobjSprite = SPR_TRE1,
+	mobjFrame = B,
 
-itemapi.addMobjAction(MT_GFZTREE, {
-	name = "pick leaves",
-	duration = 3*TICRATE,
-	animation = "shake",
+	groundAction = {
+		name = "pick berries",
+		duration = 3*TICRATE,
+		animation = "shake",
 
-	action = function(p, tree)
-		if not p.itemapi_inventory:add("leaves", 30) then return end
-		tree.state = S_ITEMAPI_GFZTREE_LEAFLESS
-	end
+		action = function(p, tree)
+			if not p.itemapi_inventory:add("berry", 10) then return end
+			itemapi.spawnGroundItem(tree.x, tree.y, tree.z, "greenflower_tree")
+			P_RemoveMobj(tree)
+		end
+	}
+})
+
+itemapi.addItem("greenflower_tree", {
+	template = "growing_plant",
+
+	name = "Greenflower tree",
+	carriable = false,
+
+	groups = { growing_plant="greenflower_berry_tree" },
+	growthTime = 3*60*TICRATE,
+
+	mobjType = MT_GFZTREE,
+	mobjSprite = SPR_TRE1,
+	mobjFrame = A,
+
+	groundAction = {
+		name = "pick leaves",
+		duration = 3*TICRATE,
+		animation = "shake",
+
+		action = function(p, tree)
+			if not p.itemapi_inventory:add("leaves", 5) then return end
+			tree.state = S_ITEMAPI_GFZTREE_LEAFLESS
+		end
+	}
+})
+
+itemapi.addItem("leafless_greenflower_tree", {
+	template = "growing_plant",
+
+	name = "leafless Greenflower tree",
+	carriable = false,
+
+	groups = { growing_plant="greenflower_berry_tree" },
+	growthTime = 3*60*TICRATE,
+
+	mobjType = MT_GFZTREE,
+	mobjState = S_ITEMAPI_GFZTREE_LEAFLESS,
+	mobjSprite = SPR_ITRE,
+	mobjFrame = A,
+
+	groundAction = {
+		name = "pick wood",
+		duration = 10*TICRATE,
+		animation = "shake",
+
+		action = function(p, tree)
+			if not p.itemapi_inventory:add("log", 5) then return end
+			P_RemoveMobj(tree)
+		end
+	}
 })
