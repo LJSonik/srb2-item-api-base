ab4a77e51e2df6181f723a050acf8222b8a9b80b
LJ Sonic <lamr@free.fr>
2025-01-10 19:20:49 +0100

    Update "sound" animation format

--- PK3/Lua/ActionAnimations/Sound.lua
+++ PK3/Lua/ActionAnimations/Sound.lua
@@ -1,13 +1,13 @@
 itemapi.addActionAnimation("sound", {
-	start = function(_, _, params, p)
-		S_StartSound(p.mo, params.sound)
+	start = function(mo, _, params)
+		S_StartSound(mo, params.sound)
 	end,
 })
 
 itemapi.addActionAnimation("looped_sound", {
-	tick = function(_, _, params, p)
+	tick = function(mo, _, params)
 		if leveltime % params.frequency == 0 then
-			S_StartSound(p.mo, params.sound)
+			S_StartSound(mo, params.sound)
 		end
 	end,
 })
